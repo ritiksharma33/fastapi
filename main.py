8560ad8ce88675ec600aca18aeb4354b8861e80a
ritiksharma33 <ritikeducationalid@gmail.com>
2026-05-23 22:02:05 +0530

    Add FastAPI application files and implement various endpoints for item and user management

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-app=FastAPI()
-@app.get('/')
-def str():
-    return{
-        "message":"server is running"
-    }
-
-
-@app.get('/{name}')
-def greeti(name:str):
-    return{
-        "name":name
-    }
-@app.get('/items/{item_id}')
-def greet(item_id:int):
-    
-    return {"id":item_id}
