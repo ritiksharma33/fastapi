be7d277a8e75fa419d7848642a4e82f4f6123d8d
ritiksharma33 <ritikeducationalid@gmail.com>
2026-05-23 23:54:34 +0530

    Implement user signup and login functionality with error handling

--- main.py
+++ main.py
@@ -1,3 +1,133 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI,Path,HTTPException
+from pydantic import BaseModel,Field
+from typing import Annotated
+from enum import Enum
+
+
+
+app=FastAPI()
+class courseName(str,Enum):
+    python="Python"
+    java="Java"
+    c="C"
+
+class StudentPublic(BaseModel):
+    id:int
+    name:str
+    course:courseName
+    fees:int
+    address: str | None = None
+    
+class StudentPrivate(StudentPublic):
+    phone:int
+    password:str
+
+class Login(BaseModel):
+    
+    gmail:str
+    password:str
+class role(str,Enum):
+    student="student"
+    admin="admin"
+
+class Signup(Login):
+    role:role
+    name:str
+
+class StudentUpdate(BaseModel):
+    fees: int | None = None
+    address: str | None = None
+
+
+
+
+students = {
+    1: {
+        "id": 1,
+        "name": "Ritik",
+        "course": "Python",
+        "fees": 50000,
+        "address": "Delhi",
+        "phone": 9999999999,
+        "password": "ritik123"
+    },
+    2: {
+        "id": 2,
+        "name": "Aman",
+        "course": "Java",
+        "fees": 45000,
+        "phone": 8888888888,
+        "password": "aman123"
+    }
+}
+
+users = {}
+
+@app.post("/signup")
+async def create_user(user:Signup):
+    for i in users.values():
+        if i["gmail"]==user.gmail:
+            raise HTTPException(
+                status_code=400,
+                detail="User already exixts"
+            )
+    new_id=len(users)+1
+    users[new_id]={
+            "id":new_id,
+            "name":user.name,
+            "gmail":user.gmail,
+            "password":user.password,
+            "role":user.role
+        }
+    return {
+            "message":"Signup successfull",
+            "user":users[new_id]
+        }
+#Login 
+@app.post("/login")
+async def login_user(user:Login):
+    for i in users.values():
+        if i['gmail']==user.gmail and i['password']==user.password:
+
+          return {
+        "message":"Login Successfull",
+        "role":i['role']
+               }
+        raise HTTPException(
+            status_code=400,
+            detail="Signup no user found"
+        )
+
+
+@app.get("/students/{id}",response_model=StudentPublic)
+async def get_student(
+    id: Annotated[int, Path(gt=0, lt=100)]
+):
+    if id not in students:
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+    return students[id]
+
+@app.get("/admin/student/{id}",response_model=StudentPrivate)
+async def get_student_private(
+    id: Annotated[int, Path(gt=0, lt=100)]
+):   
+    if id not in students:
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+    return students[id]
+
+
+@app.post("/admin/create-student",response_model=StudentPrivate)
+async def create_student(user:StudentPrivate):
+    if user.id in students:
+        raise HTTPException(
+            status_code=400,
+            detail="Student ID already exists"
+        )
+    return user[id]
 
